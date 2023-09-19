4d1a1bf8c7d2590f02ae59890e09b7e96eced9e6
Deepika <deepikardb@gmail.com>
2023-09-18 17:33:39 -0700

    Reorganized the folder structure

---
+++ StepsCounter/Main/StepsCounterApp.swift
@@ -0,0 +1,19 @@
+//
+//  StepsCounterApp.swift
+//  StepsCounter
+//
+//  Created by Deepika Ramesh on 9/18/23.
+//
+
+
+
+import SwiftUI
+
+@main
+struct StepsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
